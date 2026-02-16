37a8b327b66b77e667a90ed3a206c0fb4dcb3d55
Tasnimul Intazam Asif <intazam2896@gmail.com>
2026-02-16 17:52:46 +0600

    yolo26s

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ CAMERA_SOURCE = "rtsp://admin:boss321%23@192.168.2.42:554/cam/realmonitor?channe
 
 # YOLO model
 #YOLO_MODEL = "yolov8n.pt"
-YOLO_MODEL="yolo26n.pt"
+YOLO_MODEL="yolo26s.pt"
 
 # Detection confidence
 CONFIDENCE_THRESHOLD = 0.4
